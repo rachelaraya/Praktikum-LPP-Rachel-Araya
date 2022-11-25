4262ecfbad04262dddd11e452145d1e1e1ce66ae
rachelaraya <111627137+rachelaraya@users.noreply.github.com>
2022-11-25 18:07:33 +0700

    Add files via upload

---
+++ Praktikum LPP/Rachel Araya - Praktikum #4.py
@@ -0,0 +1,49 @@
+import random
+
+#Daftar ini memberikan gambaran tentang waktu acara.
+Kalimat_starter = random.randint(1)
+if Kalimat_starter == 1 :
+    kalimat_1 = ("Pada hari Rabu, ")
+
+
+#Daftar ini menceritakan tentang karakter utama dari cerita ini.
+karakter = random.randint(1)
+if karakter == 1 :
+    kalimat_2 = ("Rachel pergi ke PIM ")
+
+
+#Daftar ini menentukan hari yang tepat di mana beberapa insiden telah terjadi.
+waktu = random.randint(1)
+if waktu == 1 :
+    kalimat_3 = ("Ia berangkat pada jam 7 malam")
+
+
+#Daftar ini mendefinisikan plot cerita.
+story_plot = random.randint(1)
+if story_plot == 1 :
+    kalimat_4 = ("Semuanya terlihat normal saja sampai pada akhirnya ada suatu kejadian yang janggal di depan Mall, ")
+
+#Daftar ini mendefinisikan tempat di mana insiden itu terjadi.
+tempat = random.randint(1)
+if tempat == 1 :
+    kalimat_5 = ("Di depan mall ada sosok yang mengerikan ")
+
+
+#Daftar ini mendefinisikan karakter kedua dari cerita.
+second_character = random.randint(1)
+if second_character == 1 :
+    kalimat_6 = ("Sosok itu berlumuran tanah kotor ")
+
+
+#Daftar ini mendefinisikan usia karakter kedua.
+usia = random.randint(1)
+if usia == 1 :
+    kalimat_7 = ("Sosok itu tiada henti melihat ke arah saya ")
+
+
+#Daftar ini menceritakan tentang pekerjaan yang dilakukan karakter kedua.
+pekerjaan = random.randint(1)
+if pekerjaan == 1 :
+    kalimat_8 = ("Sosok itu terlihat sangat menyeramkan.")
+
+print (kalimat_1, kalimat_2, kalimat_3, kalimat_4, kalimat_5, kalimat_6, kalimat_7, kalimat_8)
